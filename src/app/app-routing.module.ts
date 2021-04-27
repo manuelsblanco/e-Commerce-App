8849d43c402ce3f625952c80f6cb8bf0ca5a996f
Manuel Blanco <blanco.manuel.sebastian@gmail.com>
2021-04-27 13:12:25 -0300

    119. Session 07

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { LogOutComponent } from './components/log-out/log-out.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {path:'',       component: HomeComponent},
+  {path:'account', component: AccountComponent},
+  {path:'cart',    component: CartComponent},
+  {path:'login',   component: LogOutComponent},
+  {path:'logout',  component: LogOutComponent},
+  {path:'orders',  component: OrdersComponent},
+  {path:'products',component: ProductsComponent},
+  {path:'signup', component: SignUpComponent},
+  {path:'**',      component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogOutComponent } from './components/log-out/log-out.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AccountComponent } from './components/account/account.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    LogOutComponent,
+    CartComponent,
+    AccountComponent,
+    OrdersComponent,
+    ProductsComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
