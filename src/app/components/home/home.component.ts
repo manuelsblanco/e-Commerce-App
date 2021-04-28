8bb07a88b7d3e603e021b805f64ed2ca4f396269
Manuel Blanco <blanco.manuel.sebastian@gmail.com>
2021-04-28 16:36:07 -0300

    122. Session 10.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../../Interface/products.interface';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  Products: Array<Product> =
+    [
+      {Name: 'Banana', Price: 3, Dscrip: 'Cavendish', ProductPath: 'assets/pic/banana.jpg'},
+      {Name: 'Kiwi', Price: 5, Dscrip: 'New Zealand Kiwi', ProductPath: 'assets/pic/kiwi.jfif'},
+      {Name: 'Orange', Price: 4, Dscrip: 'Valencians', ProductPath: 'assets/pic/orange.jfif'},
+      {Name: 'Strawberry', Price: 8, Dscrip: 'From our producers', ProductPath: 'assets/pic/strawberry.jpg'},
+    ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addToCart(index)
+  {
+    console.log("added", this.Products[index]);
   }
 
 }
