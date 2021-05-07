eb9802efe2dc4f60ed301c04ab18bbf443982bdb
Manuel Blanco <blanco.manuel.sebastian@gmail.com>
2021-05-07 17:06:42 -0300

    140. Session 28

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  shoppingCart: Array<any>;
+
+  constructor(private cart: CartService) { }
 
   ngOnInit(): void {
+    this.cart.getCart().subscribe(cs =>
+    {
+      this.shoppingCart = cs.map( x =>
+      {
+       return{
+          id: x.payload.doc.id,
+            ... // @ts-ignore
+          x.payload.doc.data() as {}
+        };
+      });
+      console.log(this.shoppingCart);
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  deleteCart(index)
+  {
+    return this.cart.deleteFromCart(this.shoppingCart[index].id);
   }
 
 }
